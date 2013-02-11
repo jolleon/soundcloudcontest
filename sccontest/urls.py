d8b3b5458bf1a9fb67da78508bc557581f34d7a3
Jules Olleon <jolleon@gmail.com>
2013-02-10 20:13:01 -0800

    static files

--- sccontest/urls.py
+++ sccontest/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -8,6 +9,8 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'sccontest.views.home', name='home'),
     url(r'^', include('contest.urls')),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
